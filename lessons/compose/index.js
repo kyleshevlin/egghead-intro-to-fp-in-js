1589de4df59e316ceecf87409968a94c5aa188b6
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-01-24 21:28:37 -0800

    update compose lesson

--- lessons/compose/index.js
+++ lessons/compose/index.js
@@ -3,18 +3,24 @@ const f = x => x + 1
 const g = x => x * 2
 const x = 3
 
-const result = f(g(x)) // 7
+const result1 = f(g(x)) // 7
 
-// Slightly more practical composition
+// Slightly more useful functions
 const scream = str => str.toUpperCase()
 const exclaim = str => `${str}!`
 const repeat = str => `${str} ${str}`
 
 const string = 'Egghead.io is awesome'
 
+// Nested
+const result2 = repeat(exclaim(scream(string)))
+// EGGHEAD.IO IS AWESOME! EGGHEAD.IO IS AWESOME!
+
 const compose = (...fns) => x =>
   fns.reduceRight((acc, fn) => fn(acc), x)
 
+// Instead of nesting, compose your functions into a new function
 const enhance = compose(repeat, exclaim, scream)
 
-enhance(string) // EGGHEAD.IO IS AWESOME! EGGHEAD.IO IS AWESOME!
+console.log(enhance(string))
+// EGGHEAD.IO IS AWESOME! EGGHEAD.IO IS AWESOME!
